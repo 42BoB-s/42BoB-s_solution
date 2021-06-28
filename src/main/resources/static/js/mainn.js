afd1d7b601014f3c0824883cda617dee6a006303
s-um <yoonms0101@gmail.com>
2021-06-28 21:50:01 +0900

    Add -alert at wrong input time(start is later than end)

--- src/main/resources/static/js/mainn.js
+++ src/main/resources/static/js/mainn.js
@@ -58,7 +58,9 @@ function getFormData() {
     menuSelect(menu_html);
     locationSelect(location_html);
 
-    if (confirm(timeFrom + "시부터 " + timeTo + "까지" + locationSet[location] + "에서" + menuSet[menu] + "예약을 하시겠습니까?")) {
+    if (timeFrom > timeTo) {
+        alert('시작 시간이 마지막 시간보다 빠릅니다');
+    } else if (confirm(timeFrom + "시부터 " + timeTo + "까지" + locationSet[location] + "에서" + menuSet[menu] + "예약을 하시겠습니까?")) {
         var json_data = {
             id: myID,
             menu: menu,
