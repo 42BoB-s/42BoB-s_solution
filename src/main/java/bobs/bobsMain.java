bd203821c9779544c73b389be9033f0b02b34917
HONGMINJU <mhong0800@gmail.com>
2021-07-26 19:08:57 +0900

    chore test https

--- src/main/java/bobs/bobsMain.java
+++ src/main/java/bobs/bobsMain.java
@@ -16,7 +16,7 @@ public class bobsMain {
 	{
 		SpringApplication.run(bobsMain.class, args);
 	}
-	@Bean
+/*	@Bean
 	public ServletWebServerFactory servletContainer() {
 		// Enable SSL Trafic
 		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
@@ -37,11 +37,10 @@ public class bobsMain {
 		return tomcat;
 	}
 
-	/*
-    We need to redirect from HTTP to HTTPS. Without SSL, this application used
-    port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
-    redirected to HTTPS on 8443.
-     */
+    //We need to redirect from HTTP to HTTPS. Without SSL, this application used
+   // port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
+   // redirected to HTTPS on 8443.
+    
 	private Connector httpToHttpsRedirectConnector() {
 		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
 		connector.setScheme("http");
@@ -50,4 +49,5 @@ public class bobsMain {
 		connector.setRedirectPort(8443);
 		return connector;
 	}
+	*/
 }
