fe99543727daa54624c656bf0d8c6f496997bfbf
HONGMINJU <mhong0800@gmail.com>
2021-07-26 17:55:28 +0900

    chore deploy.sh add kill 8443 port command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,9 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 echo "> 8080포트 kill"
 fuser -k -n tcp 8080
 sleep 5
+echo "> 8443포트 kill"
+fuser -k -n tcp 8443
+sleep 5
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
